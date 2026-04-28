4c6111ed24e71a8c64e6ac5738c12cfb5a654cdf
natetowsley <natetowsley@gmail.com>
2026-04-27 22:05:21 -0700

    Navigation Container made for map page for easier routing in future.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MapScreen from "./src/screens/MapScreen";
+
+export type RootStackParamList = {
+  Map: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <MapScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Map">
+        <Stack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
