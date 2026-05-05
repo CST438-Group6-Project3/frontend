93ca204469af56d4ec7388cb919ca77b9569db0f
natetowsley <natetowsley@gmail.com>
2026-05-04 18:19:46 -0700

    Native location picking mode added.

--- src/components/map/Map.native.tsx
+++ src/components/map/Map.native.tsx
@@ -1,12 +1,19 @@
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, MapPressEvent } from "react-native-maps";
 import type { LocationResponse } from "../../api/locations";
 
 type Props = {
     locations: LocationResponse[];
     onMarkerPress: (location: LocationResponse) => void;
+    isPickingLocation?: boolean;
+    onMapPress?: (coordinates: { lat: number; lng: number }) => void;
 };
 
-export default function HiddenGemsMap({ locations, onMarkerPress }: Props) {
+export default function HiddenGemsMap({
+    locations,
+    onMarkerPress,
+    isPickingLocation = false,
+    onMapPress,
+}: Props) {
     return (
         <MapView
             style={{ flex: 1 }}
@@ -15,6 +22,12 @@ export default function HiddenGemsMap({ locations, onMarkerPress }: Props) {
                 longitude: -121.797,
                 latitudeDelta: 0.05,
                 longitudeDelta: 0.05,
+            }}
+            onPress={(event: MapPressEvent) => {
+                if (!isPickingLocation) return;
+
+                const { latitude, longitude } = event.nativeEvent.coordinate;
+                onMapPress?.({ lat: latitude, lng: longitude });
             }}
         >
             {locations.map((location) => (
