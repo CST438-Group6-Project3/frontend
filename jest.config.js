48406d88e62d4aeb26054514d0a07b99856ff915
natetowsley <natetowsley@gmail.com>
2026-05-04 19:50:17 -0700

    jest installed, .gitignore added to avoid committing test reports. May overlap an existing PR currently.

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+      tsconfig: {
+        jsx: "react-jsx",
+      },
+      },
+    ],
+  },
+  moduleFileExtensions: ["web.tsx", "web.ts", "tsx", "ts", "jsx", "js"],
+  testMatch: ["<rootDir>/src/**/*.test.ts", "<rootDir>/src/**/*.test.tsx"],
+  collectCoverageFrom: [
+    "src/api/**/*.{ts,tsx}",
+    "src/components/location/AddSpotSheet.web.tsx",
+    "!src/**/*.native.{ts,tsx}",
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 80,
+      statements: 80,
+      functions: 75,
+      branches: 70,
+    },
+  },
+};
